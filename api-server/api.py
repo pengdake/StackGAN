6a265b6d1631394653423a4cbd4481b4a8907041
pengdake <19921207pq@gmail.com>
2019-10-27 13:56:17 +0800

    update some file path

--- api-server/api.py
+++ api-server/api.py
@@ -32,17 +32,18 @@ def detect():
     # save file to Data/flowers/example_captions.txt
     print "start save txt"
     example_file = request.files["file"]
-    caption_path = "/mxtg/code/StackGAN/Data/flowers/example_captions_%s.txt" % uid
+    base_path = os.path.dirname(os.path.dirname(__file__))
+    caption_path = "%s/Data/flowers/example_captions_%s.txt" % (base_path, uid)
     example_file.save(caption_path)
     print "start txt transform"
     # transform  Data/flowers/example_captions.txt to Data/flowers/example_captions.t7
-    s, o = commands.getstatusoutput("sh /mxtg/code/StackGAN/demo/flowers_demo.sh %s" % caption_path.split(".")[0])
+    s, o = commands.getstatusoutput("sh %s/demo/flowers_demo.sh %s" % (base_path, caption_path.split(".")[0]))
     if s != 0:
         # transfrom failed
         return make_response(o, 400)
     # text to image
     print "start create img"
-    s, o = commands.getstatusoutput("python /mxtg/code/StackGAN/demo/demo.py --model_path %s --uid %s" % (MODEL_PATH, uid))
+    s, o = commands.getstatusoutput("python %s/demo/demo.py --model_path %s --uid %s" % (base_path, MODEL_PATH, uid))
     if s != 0:
         return make_response(o, 400)
     cache.set("training", "false")
@@ -50,7 +51,7 @@ def detect():
     print "start img transform"
     response_data = {}
     response_data["type"] = "img"
-    img_path = "/mxtg/code/StackGAN/Data/flowers/example_captions_%s/sentence0.jpg" % uid
+    img_path = "%s/Data/flowers/example_captions_%s/sentence0.jpg" % (base_path, uid)
     with open(img_path, "rb") as f:
         response_data["data"] = "data:image/jpg;base64,%s" % base64.b64encode(f.read())
     # return base64 code
